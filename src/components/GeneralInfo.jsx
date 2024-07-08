56d385dea7df60c3a2fe55ba397116ea2ade9a98
ruiz.terce <ruiz.terce@gmail.com>
2024-07-08 14:19:41 +0200

    Update GeneralInfo.jsx to first working version
    This component shows general information of the
    applicant. Initially it pulls data from data.js
    and renders it in html elements. It includes a
    edit button that renders input elements to allow
    the user modifying each info field and either save
    or cancel the edits.

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,3 +1,67 @@
+import { useState } from "react";
+import data from "../data/data.js";
+
 export default function GeneralInfo() {
-  return <span>Hello World</span>;
+  const [name, setName] = useState(data.name);
+  const [email, setEmail] = useState(data.email);
+  const [phone, setPhone] = useState(data.phone);
+  const [editMode, setEditMode] = useState(false);
+
+  function toggleEdit() {
+    setEditMode(!editMode);
+  }
+
+  function saveEdit(e) {
+    e.preventDefault();
+    data.name = name;
+    data.email = email;
+    data.phone = phone;
+    toggleEdit();
+  }
+
+  function cancelEdit() {
+    setName(data.name);
+    setEmail(data.email);
+    setPhone(data.phone);
+    toggleEdit();
+  }
+
+  if (!editMode) {
+    return (
+      <>
+        <button onClick={toggleEdit}>Edit</button>
+        <p>{name}</p>
+        <p>{email}</p>
+        <p>{phone}</p>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <form onSubmit={saveEdit}>
+          <button type="Submit">Save</button>
+          <button type="button" onClick={cancelEdit}>
+            Cancel
+          </button>
+          <p>Edit Mode</p>
+
+          <input
+            className="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+          <input
+            className="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+          <input
+            className="phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          ></input>
+        </form>
+      </>
+    );
+  }
 }
